1201006836f788c32459a62a9f5a55448994b8b7
Chris Yancey <cyancey1@gmail.com>
2014-06-16 00:55:18 -0700

    created route to obtain weather information via WeatherUnderground

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   root 'home#index'
   post 'contact_email', to: 'contact#email'
+  get 'weather_conditions', to: 'weather#current_conditions'
 
 
   # The priority is based upon order of creation: first created -> highest priority.
